205ac77dde3532a9da82a03084cf89e9d53532cf
Deche <csharper@gmail.com>
2021-08-04 08:32:29 +0700

    first commit

---
+++ test/sample-test.js
@@ -0,0 +1,29 @@
+const { expect, assert } = require("chai");
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+
+    // wait until the transaction is mined
+    await setGreetingTx.wait();
+
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+
+  it("Should modify x", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+
+    await greeter.modify(25);
+    const x = await greeter.callStatic.x();
+
+    assert.equal(x, 25);
+  });  
+});
